fc2d6e685bcf049bbc9ef36e8f44c49cc3a9e0c9
esraa <esraashammout788@gmail.com>
2025-06-03 20:43:16 +0300

    اضافة واجهة تجسل الدخول وال splashScreen

--- lib/features/auth/presentation/views/sign_in_view.dart
+++ lib/features/auth/presentation/views/sign_in_view.dart
@@ -1,11 +1,215 @@
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
-import 'package:med_tech_admin/features/auth/presentation/views/widgets/sign_in_view_body.dart';
 
-class SignInView extends StatelessWidget {
+class SignInView extends StatefulWidget {
   const SignInView({super.key});
 
   @override
+  State<SignInView> createState() => _LoginScreenState();
+}
+
+class _LoginScreenState extends State<SignInView> with SingleTickerProviderStateMixin {
+  bool _obscureText = true;
+
+  @override
+  void _togglePasswordVisibility() {
+    setState(() {
+      _obscureText = !_obscureText;
+    });
+  }
+  @override
   Widget build(BuildContext context) {
-    return const Scaffold(body: SignInViewBody());
+    return Container(
+      decoration: BoxDecoration(
+        gradient: LinearGradient(
+          begin: Alignment.centerLeft,
+          end: Alignment.centerRight,
+          colors: [
+            Theme.of(context).colorScheme.secondary,
+            Colors.white,
+          ],
+        ),
+      ),
+      child: Scaffold(
+        backgroundColor: Colors.transparent,
+        body: Center(
+          child: Container(
+            width: 900,
+            height: 500,
+            decoration: BoxDecoration(
+              color: Colors.white,
+              borderRadius: BorderRadius.circular(10),
+              boxShadow: [
+                BoxShadow(
+                  color: Colors.black.withOpacity(0.2),
+                  blurRadius: 20,
+                  offset: const Offset(0, 10),
+                ),
+              ],
+            ),
+            child: Row(
+              children: [
+                Expanded(
+                  child: Stack(
+                    children: [
+                      Opacity(
+                        opacity: 0.4,
+                        child: ClipRRect(
+                          borderRadius: const BorderRadius.only(
+                            topLeft: Radius.circular(0),
+                            bottomLeft: Radius.circular(0),
+                          ),
+                          child: Image.asset(
+                            'assets/images/login.jpg',
+                            width: double.infinity,
+                            height: double.infinity,
+                            fit: BoxFit.cover,
+                          ),
+                        ),
+                      ),
+                      Center(
+                        child: Column(
+                          mainAxisAlignment: MainAxisAlignment.center,
+                          crossAxisAlignment: CrossAxisAlignment.center,
+                          children: [
+                            // Animated text with falling effect
+                            Text(
+                              'مرحباً بكم ',
+                              style: TextStyle(
+                                fontSize: 32,
+                                color: Theme.of(context).colorScheme.primary,
+                                fontWeight: FontWeight.bold,
+                                shadows: [
+                                  Shadow(
+                                    blurRadius: 10,
+                                    color: Colors.black.withOpacity(0.4),
+                                    offset: const Offset(2, 2),
+                                  ),
+                                ],
+                              ),
+                            ),
+
+                            Text(
+                              ' BitarMed  ',
+                              style: TextStyle(
+                                fontSize: 32,
+                                color: Color(0xFF022015),
+                                fontWeight: FontWeight.bold,
+                                shadows: [
+                                  Shadow(
+                                    blurRadius: 10,
+                                    color: Theme.of(context).colorScheme.primary.withOpacity(0.4),
+                                    offset: const Offset(2, 2),
+                                  ),
+                                ],
+                              ),
+                            ),
+                            Text(
+                                ' لبيع وتاجير وصيانة ',
+                                style: TextStyle(
+                                  fontSize: 32,
+                                  color: Color(0xFF022015),
+                                  fontWeight: FontWeight.bold,
+                                  shadows: [
+                                    Shadow(
+                                      blurRadius: 10,
+                                      color: Theme.of(context).colorScheme.primary.withOpacity(0.4),
+                                      offset: const Offset(2, 2),
+                                    ),
+                                  ],
+                                ),
+                              ),
+                            Text(
+                                ' الاجهزة الطبية  ',
+                                style: TextStyle(
+                                  fontSize: 32,
+                                  color: Theme.of(context).colorScheme.primary,
+                                  fontWeight: FontWeight.bold,
+                                  shadows: [
+                                    Shadow(
+                                      blurRadius: 10,
+                                      color: Colors.black.withOpacity(0.4),
+                                      offset: const Offset(2, 2),
+                                    ),
+                                  ],
+                                ),
+                              ),
+                          ],
+                        ),
+                      ),
+                    ],
+                  ),
+                ),
+
+                Expanded(
+                  child: Padding(
+                    padding: const EdgeInsets.symmetric(horizontal: 32, vertical: 40),
+                    child: Column(
+                      mainAxisAlignment: MainAxisAlignment.center,
+                      children: [
+                        Text(
+                          'تسجيل الدخول',
+                          style: TextStyle(
+                            fontSize: 24,
+                            fontWeight: FontWeight.bold,
+                            color: Theme.of(context).colorScheme.primary,
+                          ),
+                        ),
+                        const SizedBox(height: 20),
+                        TextFormField(
+                          decoration: InputDecoration(
+                            labelText: 'البريد الإلكتروني',
+                            border: OutlineInputBorder(
+                              borderRadius: BorderRadius.circular(12),
+                            ),
+                          ),
+                        ),
+                        const SizedBox(height: 16),
+                        // Password field with visibility toggle
+                        TextFormField(
+                          obscureText: _obscureText,
+                          decoration: InputDecoration(
+                            labelText: 'كلمة المرور',
+                            border: OutlineInputBorder(
+                              borderRadius: BorderRadius.circular(12),
+                            ),
+                            suffixIcon: IconButton(
+                              icon: Icon(
+                                _obscureText ? Icons.visibility_off : Icons.visibility,
+                                color: Theme.of(context).colorScheme.primary,
+                              ),
+                              onPressed: _togglePasswordVisibility,
+                            ),
+                          ),
+                        ),
+                        const SizedBox(height: 24),
+                        SizedBox(
+                          width: double.infinity,
+                          height: 50,
+                          child: ElevatedButton(
+                            style: ElevatedButton.styleFrom(
+                              backgroundColor: Theme.of(context).colorScheme.primary,
+                              shape: RoundedRectangleBorder(
+                                borderRadius: BorderRadius.circular(12),
+                              ),
+                            ),
+                            onPressed: () {
+                            },
+                            child: Text(
+                              'تسجيل الدخول',
+                              style: TextStyle(fontSize: 18, color: Theme.of(context).colorScheme.tertiary),
+                            ),
+                          ),
+                        ),
+                      ],
+                    ),
+                  ),
+                ),
+              ],
+            ),
+          ),
+        ),
+      ),
+    );
   }
 }
--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:med_tech_admin/SplashScreen.dart';
 import 'package:med_tech_admin/features/auth/presentation/views/sign_in_view.dart';
 
 void main() {
@@ -10,6 +11,36 @@ class MedTech extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return const MaterialApp(home: SignInView());
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      title: 'BitarMed',
+      theme: ThemeData(
+        colorScheme: ColorScheme.fromSeed(
+          secondary:  Color(0xff0A9F68),
+          primary:  Color(0xFF022015),
+          seedColor:  Color(0xFF09AF72),
+          tertiary:  Color(0xFF6C9E8C),
+          background: Colors.white,
+        ),
+        useMaterial3: true,
+        fontFamily: 'Tajawal',
+        appBarTheme: const AppBarTheme(
+          backgroundColor:Color(0xFF022015),
+          elevation: 0,
+          centerTitle: true,
+          iconTheme: IconThemeData(color: Color(0xff0A9F68)),
+          titleTextStyle: TextStyle(
+            color: Color(0xFF022015),
+            fontSize: 20,
+            fontWeight: FontWeight.bold,
+            fontFamily: 'Tajawal',
+          ),
+        ),
+      ),
+      home: const SplashScreen(),
+      locale: const Locale('ar', 'SA'),
+    );
   }
+
 }
+
